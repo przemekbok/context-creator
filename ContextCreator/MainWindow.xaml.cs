872ba38d863b57200ddfc8ced4f19797ddd45f82
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-05-05 21:24:12 +0200

    Add event handlers for TreeView selection

--- ContextCreator/MainWindow.xaml.cs
+++ ContextCreator/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using ContextCreator.Models;
 using ContextCreator.Services;
 using ContextCreator.ViewModels;
@@ -13,6 +15,7 @@ namespace ContextCreator
     {
         private readonly MainViewModel _viewModel;
         private readonly DialogService _dialogService;
+        private object _lastSelectedItem;
 
         public MainWindow()
         {
@@ -24,15 +27,59 @@ namespace ContextCreator
             // Create view model
             _viewModel = new MainViewModel();
             DataContext = _viewModel;
-            
-            // Subscribe to tree view events
-            FolderTreeView.SelectedItemChanged += FolderTreeView_SelectedItemChanged;
         }
 
         private void FolderTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            // Update preview content
-            _viewModel.UpdatePreview(e.NewValue);
+            if (e.NewValue != null)
+            {
+                _lastSelectedItem = e.NewValue;
+                
+                // Update preview content
+                _viewModel.UpdatePreview(e.NewValue);
+            }
+        }
+        
+        private void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            if (sender is TreeViewItem treeViewItem)
+            {
+                // Prevent the event from bubbling up to parent TreeViewItems
+                e.Handled = true;
+                
+                // Force the selection to stay with this item
+                treeViewItem.IsSelected = true;
+                
+                // Get the actual data item bound to this TreeViewItem
+                var item = treeViewItem.DataContext;
+                
+                // Update the preview with the actual item
+                if (item != null)
+                {
+                    _viewModel.UpdatePreview(item);
+                }
+            }
+        }
+        
+        private void TreeViewItem_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TreeViewItem treeViewItem)
+            {
+                // Make sure this item gets selected
+                treeViewItem.IsSelected = true;
+                
+                // Get the actual data item bound to this TreeViewItem
+                var item = treeViewItem.DataContext;
+                
+                // Update the preview with the actual item
+                if (item != null)
+                {
+                    _viewModel.UpdatePreview(item);
+                }
+                
+                // Mark event as handled to prevent it from bubbling up
+                e.Handled = true;
+            }
         }
 
         private void ContentFilterMenuItem_Click(object sender, RoutedEventArgs e)
