5a4606e428e8f420fb1ba86583b9a9b1861d4fcb
Przemek <34289616+przemekbok@users.noreply.github.com>
2025-05-05 16:50:37 +0200

    Add ViewModel classes for WPF implementation

---
+++ ContextCreator/ViewModels/BaseViewModel.cs
@@ -0,0 +1,43 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ContextCreator.ViewModels
+{
+    /// <summary>
+    /// Base class for all view models providing property change notification
+    /// </summary>
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Event raised when a property value changes
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Raises the PropertyChanged event
+        /// </summary>
+        /// <param name="propertyName">Name of the property that changed</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Sets a property value and raises the PropertyChanged event if the value has changed
+        /// </summary>
+        /// <typeparam name="T">Type of the property</typeparam>
+        /// <param name="field">Reference to the backing field</param>
+        /// <param name="value">New value</param>
+        /// <param name="propertyName">Name of the property</param>
+        /// <returns>True if the value was changed, false otherwise</returns>
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(field, value))
+                return false;
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ ContextCreator/ViewModels/FilterViewModel.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Windows.Input;
+using ContextCreator.Commands;
+using ContextCreator.Models;
+
+namespace ContextCreator.ViewModels
+{
+    /// <summary>
+    /// View model for the filter dialog
+    /// </summary>
+    public class FilterViewModel : BaseViewModel
+    {
+        private FilterType _filterType = FilterType.FileName;
+        private string _filterExpression = "";
+        private bool _isCaseSensitive;
+        private bool _isRegex;
+        private FilterAction _filterAction = FilterAction.Include;
+        private bool _applyToSelectedFoldersOnly;
+
+        /// <summary>
+        /// Gets or sets the filter type
+        /// </summary>
+        public FilterType FilterType
+        {
+            get => _filterType;
+            set => SetProperty(ref _filterType, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the filter expression
+        /// </summary>
+        public string FilterExpression
+        {
+            get => _filterExpression;
+            set => SetProperty(ref _filterExpression, value);
+        }
+
+        /// <summary>
+        /// Gets or sets whether the filter is case sensitive
+        /// </summary>
+        public bool IsCaseSensitive
+        {
+            get => _isCaseSensitive;
+            set => SetProperty(ref _isCaseSensitive, value);
+        }
+
+        /// <summary>
+        /// Gets or sets whether the filter uses regular expressions
+        /// </summary>
+        public bool IsRegex
+        {
+            get => _isRegex;
+            set => SetProperty(ref _isRegex, value);
+        }
+
+        /// <summary>
+        /// Gets or sets whether the filter includes or excludes matching files
+        /// </summary>
+        public FilterAction FilterAction
+        {
+            get => _filterAction;
+            set => SetProperty(ref _filterAction, value);
+        }
+
+        /// <summary>
+        /// Gets or sets whether the filter applies only to selected folders
+        /// </summary>
+        public bool ApplyToSelectedFoldersOnly
+        {
+            get => _applyToSelectedFoldersOnly;
+            set => SetProperty(ref _applyToSelectedFoldersOnly, value);
+        }
+
+        /// <summary>
+        /// Gets the command to apply the filter
+        /// </summary>
+        public ICommand ApplyCommand { get; }
+
+        /// <summary>
+        /// Gets the command to cancel the filter operation
+        /// </summary>
+        public ICommand CancelCommand { get; }
+
+        /// <summary>
+        /// Event raised when the filter is applied
+        /// </summary>
+        public event EventHandler<FilterOptions>? FilterApplied;
+
+        /// <summary>
+        /// Event raised when the filter operation is canceled
+        /// </summary>
+        public event EventHandler? FilterCanceled;
+
+        /// <summary>
+        /// Initializes a new instance of the FilterViewModel class
+        /// </summary>
+        public FilterViewModel()
+        {
+            ApplyCommand = new RelayCommand(_ => ExecuteApply());
+            CancelCommand = new RelayCommand(_ => ExecuteCancel());
+        }
+
+        private void ExecuteApply()
+        {
+            var options = new FilterOptions
+            {
+                Type = FilterType,
+                Expression = FilterExpression,
+                IsCaseSensitive = IsCaseSensitive,
+                IsRegex = IsRegex,
+                Action = FilterAction,
+                ApplyToSelectedFoldersOnly = ApplyToSelectedFoldersOnly
+            };
+
+            FilterApplied?.Invoke(this, options);
+        }
+
+        private void ExecuteCancel()
+        {
+            FilterCanceled?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
